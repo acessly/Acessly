59aca3f06443b6394f5dd2c4982f07f31b7b8e45
GabrielRossi01 <rossi17006@gmail.com>
2025-11-16 13:53:29 -0300

    Criando pacote Exceptions dentro da camada Application para centralizar exceções de regra de negócio

---
+++ Acessly/Application/Exceptions/BusinessException.cs
@@ -0,0 +1,8 @@
+﻿namespace Acessly.Application.Exceptions
+{
+    public class BusinessException : Exception
+    {
+        public BusinessException(string message) : base(message) { }
+        public BusinessException(string message, Exception inner) : base(message, inner) { }
+    }
+}
